a5c2f62d4224620cc2ef3dd739bf88b54d046f6c
HSCHETAN12 <hschetan49@gmail.com>
2024-12-17 18:36:27 +0530

    CRUD completed

--- src/main/java/org/Ey/RestCrudApi/Service/ProductService.java
+++ src/main/java/org/Ey/RestCrudApi/Service/ProductService.java
@@ -96,4 +96,55 @@ public class ProductService {
 		}
 	}
 
+	public ResponseEntity<Object> DeleteById(int id) {
+		Optional<Product> product=repository.findById(id);
+		Map<String, Object> map=new HashMap<String, Object>();
+		if(product.isEmpty())
+		{
+			map.put("message","Products Not found");
+			return new ResponseEntity<Object>(map,HttpStatus.NOT_FOUND);
+		}
+		else {
+			repository.deleteById(id);
+			map.put("message","Product Deleted");
+			return new ResponseEntity<Object>(map,HttpStatus.OK);
+		}
+	}
+
+	public ResponseEntity<Object> UpdateById(Product product) {
+		repository.save(product);
+		Map<String, Object> map=new HashMap<String, Object>();
+		map.put("message", "product Updated Sucess");
+		return new ResponseEntity<Object>(map, HttpStatus.CREATED);
+	}
+
+	public ResponseEntity<Object> UpdateByValue(int id, Product product) {
+		Optional<Product> optional =repository.findById(id);
+		Map<String, Object> map=new HashMap<String, Object>();
+		if(optional.isEmpty())
+		{
+			map.put("message","Products Not found");
+			return new ResponseEntity<Object>(map,HttpStatus.NOT_FOUND);
+		}
+		else {
+			Product existingProduct=optional.get();
+			if(product.getName()!=null)
+				existingProduct.setName(product.getName());
+			if(product.getDescription()!=null)
+				existingProduct.setDescription(product.getDescription());
+			if(product.getPrice()!=0)
+				existingProduct.setPrice(product.getPrice());
+			if(product.getStack()!=0)
+				existingProduct.setStack(product.getStack());
+			repository.save(existingProduct);
+			
+			map.put("message","Product get updated");
+			
+			return new ResponseEntity<Object>(map,HttpStatus.OK);
+			
+		}
+		
+		
+	}
+
 }
--- src/main/java/org/Ey/RestCrudApi/controller/Controller.java
+++ src/main/java/org/Ey/RestCrudApi/controller/Controller.java
@@ -6,9 +6,12 @@ import org.Ey.RestCrudApi.Service.ProductService;
 import org.Ey.RestCrudApi.dto.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +55,24 @@ public class Controller {
 	public ResponseEntity<Object> fetchByname(@PathVariable int min,@PathVariable int max)
 	{
 		return service.fetchByPriceBetween(min,max);
+	}
+	
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<Object> fetchByname(@PathVariable int id)
+	{
+		return service.DeleteById(id);
+	}
+	
+	@PutMapping("/products")
+	public ResponseEntity<Object> UpdateProduct(@RequestBody Product product)
+	{
+		return service.UpdateById(product);
+	}
+	
+	@PatchMapping("/products/{id}")
+	public ResponseEntity<Object> UpdateProductId(@PathVariable int id, @RequestBody Product product)
+	{
+		return service.UpdateByValue(id,product);
 	}
 	
 }
